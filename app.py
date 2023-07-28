bea42e0e46b591aafcc75e499104e938c73f03e7
Sai Shibu N B <saishibu38380@gmail.com>
2023-07-28 17:37:17 +0530

    u

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import os
 import json
 from flask import Flask, render_template, request
-import matplotlib.pyplot as plt
-import io
-import base64
+import plotly.graph_objects as go
 
 app = Flask(__name__)
 
@@ -24,7 +22,7 @@ def load_data(filename):
             return json.load(file)
     return []
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/purchase', methods=['POST'])
 def milk_purchase():
     if request.method == 'POST':
         date = request.form['date']
@@ -36,6 +34,10 @@ def milk_purchase():
         purchases.append({'date': date, 'quantity': quantity, 'total_cost': total_cost})
         save_data(purchases, purchase_data_file)
 
+    return '', 204
+
+@app.route('/payment', methods=['POST'])
+def add_payment():
     if request.method == 'POST':
         date = request.form['date']
         amount = float(request.form['amount'])
@@ -45,6 +47,10 @@ def milk_purchase():
         payments.append({'date': date, 'amount': amount})
         save_data(payments, payment_data_file)
 
+    return '', 204
+
+@app.route('/')
+def display_data():
     # Load data for display
     purchases = load_data(purchase_data_file)
     payments = load_data(payment_data_file)
@@ -53,34 +59,44 @@ def milk_purchase():
     total_quantity = sum(item['quantity'] for item in purchases)
     total_cost = sum(item['total_cost'] for item in purchases)
 
-    # Prepare data for the graph
+    # Prepare data for the graphs
     purchase_dates = [item['date'] for item in purchases]
     quantities = [item['quantity'] for item in purchases]
     costs = [item['total_cost'] for item in purchases]
 
-    # Generate the graph and get the base64 string
-    # graph = generate_graph(purchase_dates, quantities, costs)
+    # Generate the graphs
+    quantity_graph = generate_quantity_graph(purchase_dates, quantities)
+    cost_graph = generate_cost_graph(purchase_dates, costs)
 
-    # Render the template with the graph and data
+    # Render the template with the graphs and data
     return render_template('milk_purchase.html', purchases=purchases, payments=payments,
-                           total_quantity=total_quantity, total_cost=total_cost)
+                           total_quantity=total_quantity, total_cost=total_cost,
+                           quantity_graph=quantity_graph, cost_graph=cost_graph)
 
-# def generate_graph(dates, quantities, costs):
-#     # ... (previous code, unchanged)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-    # graph = generate_graph(dates, quantities, costs)
-
-    # # Render the template with the graph
-    # return render_template('milk_purchase.html', purchases=purchases, total_quantity=total_quantity, total_cost=total_cost, graph=graph)
-
-@app.route('/clear', methods=['POST'])
+@app.route('/clear_purchase', methods=['POST'])
 def clear_purchase_history():
-    # Clear the purchase history by overwriting the data file
-    save_data([])
+    # Clear purchase history by saving an empty list to the file
+    save_data([], purchase_data_file)
     return '', 204
 
+@app.route('/clear_payment', methods=['POST'])
+def clear_payment_history():
+    # Clear payment history by saving an empty list to the file
+    save_data([], payment_data_file)
+    return '', 204
+
+def generate_quantity_graph(dates, quantities):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=dates, y=quantities, mode='lines+markers', name='Milk Quantity (liters)'))
+    fig.update_layout(title='Milk Quantity vs Date', xaxis_title='Date', yaxis_title='Milk Quantity (liters)')
+    return fig.to_html()
+
+def generate_cost_graph(dates, costs):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=dates, y=costs, mode='lines+markers', name='Total Cost (Rs)', line=dict(color='red')))
+    fig.update_layout(title='Total Cost vs Date', xaxis_title='Date', yaxis_title='Total Cost (Rs)')
+    return fig.to_html()
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
